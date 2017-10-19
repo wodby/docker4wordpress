e060f4e36210122da22def9e242a8d48b4d0ede2
Chingis Sandanov <chingis@wodby.com>
2017-10-19 19:03:24 +0700

    Temporary remove nginx health check

--- test/4/7.1/run.sh
+++ test/4/7.1/run.sh
@@ -8,9 +8,9 @@ fi
 
 . ../../images.env
 
+# TODO: return health checks
 docker-compose up -d
 docker-compose exec mariadb make check-ready max_try=12 wait_seconds=3 -f /usr/local/bin/actions.mk
-docker-compose exec nginx make check-ready -f /usr/local/bin/actions.mk
 docker-compose exec php make check-ready -f /usr/local/bin/actions.mk
 docker-compose exec php ./test.sh
 docker-compose down
