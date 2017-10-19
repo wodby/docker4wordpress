ce51efd9221ff2a0fd4096709dd4c2db30fda857
Chingis Sandanov <chingis@wodby.com>
2017-10-19 19:13:38 +0700

    Tests fix

--- test/4/7.0/test.sh
+++ test/4/7.0/test.sh
@@ -8,6 +8,8 @@ fi
 
 WP_DOMAIN="$( echo "${BASE_URL}" | sed 's/https\?:\/\///' )"
 
+make init -f /usr/local/bin/actions.mk
+
 wp core install --url="${BASE_URL}" --title="WordPress 4" \
     --admin_user=admin --admin_password=admin --admin_email=admin@example.com
 
