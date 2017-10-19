439df4c67bfa35c752f696e11423139c8f54be64
Chingis Sandanov <chingis@wodby.com>
2017-10-19 19:08:16 +0700

    Fix tests

--- test/4/7.0/run.sh
+++ test/4/7.0/run.sh
@@ -12,5 +12,6 @@ fi
 docker-compose up -d
 docker-compose exec mariadb make check-ready max_try=12 wait_seconds=3 -f /usr/local/bin/actions.mk
 docker-compose exec php make check-ready -f /usr/local/bin/actions.mk
+docker-compose exec --user=0 php chown -R www-data:www-data /var/www/html
 docker-compose exec php ./test.sh
 docker-compose down
