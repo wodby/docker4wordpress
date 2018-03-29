37e768254549a1faa13535fae78efec030b6c0c5
Chingis Sandanov <chingis@wodby.com>
2018-03-29 12:48:19 +0700

    Add make commands: wp and logs

--- docker.mk
+++ docker.mk
@@ -1,8 +1,10 @@
 include .env
 
-.PHONY: up down stop prune ps shell
+.PHONY: up down stop prune ps shell wp logs
 
 default: up
+
+WP_ROOT ?= /var/www/html/
 
 up:
 	@echo "Starting up containers for for $(PROJECT_NAME)..."
@@ -24,3 +26,13 @@ ps:
 
 shell:
 	docker exec -ti $(shell docker ps --filter name='$(PROJECT_NAME)_php' --format "{{ .ID }}") sh
+
+wp:
+	docker exec $(shell docker ps --filter name='$(PROJECT_NAME)_php' --format "{{ .ID }}") wp --path=$(WP_ROOT) $(filter-out $@,$(MAKECMDGOALS))
+
+logs:
+	@docker-compose logs -f $(filter-out $@,$(MAKECMDGOALS))
+
+# https://stackoverflow.com/a/6273809/1826109
+%:
+	@:
