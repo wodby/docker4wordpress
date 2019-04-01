c873c1ee23c1f5c0205f76e423f8f0bb73dadb56
Chingis Sandanov <chingis@wodby.com>
2019-04-01 16:44:28 +0700

    Add make help command, allow prune specific services, add make start

--- docker.mk
+++ docker.mk
@@ -6,30 +6,54 @@ default: up
 
 WP_ROOT ?= /var/www/html/
 
+## help	:	Print commands help.
+help : docker.mk
+	@sed -n 's/^##//p' $<
+
+## up	:	Start up containers.
 up:
 	@echo "Starting up containers for for $(PROJECT_NAME)..."
 	docker-compose pull
 	docker-compose up -d --remove-orphans
 
+## down	:	Stop containers.
 down: stop
 
+## start	:	Start containers without updating.
+start:
+	@echo "Starting containers for $(PROJECT_NAME) from where you left off..."
+	@docker-compose start
+
+## stop	:	Stop containers.
 stop:
 	@echo "Stopping containers for $(PROJECT_NAME)..."
 	@docker-compose stop
 
+## prune	:	Remove containers and their volumes.
+##		You can optionally pass an argument with the service name to prune single container
+##		prune mariadb	: Prune `mariadb` container and remove its volumes.
+##		prune mariadb solr	: Prune `mariadb` and `solr` containers and remove their volumes.
 prune:
 	@echo "Removing containers for $(PROJECT_NAME)..."
-	@docker-compose down -v
+	@docker-compose down -v $(filter-out $@,$(MAKECMDGOALS))
 
+## ps	:	List running containers.
 ps:
 	@docker ps --filter name='$(PROJECT_NAME)*'
 
+## shell	:	Access `php` container via shell.
 shell:
 	docker exec -ti -e COLUMNS=$(shell tput cols) -e LINES=$(shell tput lines) $(shell docker ps --filter name='$(PROJECT_NAME)_php' --format "{{ .ID }}") sh
 
+## wp	:	Executes `wp cli` command in a specified `WP_ROOT` directory (default is `/var/www/html/`).
+## 		Doesn't support --flag arguments.
 wp:
 	docker exec $(shell docker ps --filter name='^/$(PROJECT_NAME)_php' --format "{{ .ID }}") wp --path=$(WP_ROOT) $(filter-out $@,$(MAKECMDGOALS))
 
+## logs	:	View containers logs.
+##		You can optinally pass an argument with the service name to limit logs
+##		logs php	: View `php` container logs.
+##		logs nginx php	: View `nginx` and `php` containers logs.
 logs:
 	@docker-compose logs -f $(filter-out $@,$(MAKECMDGOALS))
 
