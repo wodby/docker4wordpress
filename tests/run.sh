74a13e9694008a45a786bf449b638a9db36cce37
Chingis Sandanov <chingis@wodby.com>
2019-10-30 14:25:31 +0700

    Increase varnish startup max tries in tests

--- tests/run.sh
+++ tests/run.sh
@@ -9,7 +9,7 @@ fi
 docker-compose up -d
 docker-compose exec nginx make check-ready -f /usr/local/bin/actions.mk
 docker-compose exec php make check-ready -f /usr/local/bin/actions.mk
-docker-compose exec varnish make check-ready -f /usr/local/bin/actions.mk
+docker-compose exec varnish make check-ready max_try=5 -f /usr/local/bin/actions.mk
 docker-compose exec mariadb make check-ready max_try=12 wait_seconds=3 -f /usr/local/bin/actions.mk
 docker-compose exec php tests.sh
 docker-compose down
