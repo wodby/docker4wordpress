ac858d06f680e8491eff4a56ed3217e68563978d
Chingis Sandanov <chingis@wodby.com>
2019-12-16 10:14:01 +0700

    Add PHP 7.4

--- tests/tests.sh
+++ tests/tests.sh
@@ -14,7 +14,7 @@ wp core install --url="${BASE_URL}" --title="WordPress 4" \
     --admin_user=admin --admin_password=admin --admin_email=admin@example.com
 
 # TODO: test redis and varnish.
-#wp plugin install redis-cache --activate
+wp plugin install redis-cache --activate
 wp plugin install vcaching --activate
 
 echo -n "Checking WordPress homepage... "
