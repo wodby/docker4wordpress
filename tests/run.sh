1dfa114d98796e20fba7b636acb3a2f9f888a84d
Chingis Sandanov <chingis@wodby.com>
2023-12-08 12:35:43 +0300

    Debug

--- tests/run.sh
+++ tests/run.sh
@@ -7,9 +7,9 @@ if [[ -n "${DEBUG}" ]]; then
 fi
 
 docker-compose up -d
-docker-compose exec -T nginx make check-ready -f /usr/local/bin/actions.mk
+docker-compose exec -T nginx make check-ready max_try=5 wait_seconds=3 -f /usr/local/bin/actions.mk
 docker-compose exec -T php make check-ready max_try=5 wait_seconds=3 -f /usr/local/bin/actions.mk
 docker-compose exec -T varnish make check-ready max_try=5 -f /usr/local/bin/actions.mk
-docker-compose exec -T mariadb make check-ready max_try=12 wait_seconds=3 -f /usr/local/bin/actions.mk
+docker-compose exec -T mariadb make check-ready max_try=12 wait_seconds=5 -f /usr/local/bin/actions.mk
 docker-compose exec -T php tests.sh
 docker-compose down
