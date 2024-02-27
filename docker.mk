e7ee1593455be24f0ea7689f0ba13bf51e262b97
Chingis Sandanov <chingis@wodby.com>
2024-02-27 17:00:07 +0300

    Rename docker-compose to compose

--- docker.mk
+++ docker.mk
@@ -13,8 +13,8 @@ help : docker.mk
 ## up	:	Start up containers.
 up:
 	@echo "Starting up containers for $(PROJECT_NAME)..."
-	docker-compose pull
-	docker-compose up -d --remove-orphans
+	docker compose pull
+	docker compose up -d --remove-orphans
 
 mutagen:
 	mutagen-compose up
@@ -25,12 +25,12 @@ down: stop
 ## start	:	Start containers without updating.
 start:
 	@echo "Starting containers for $(PROJECT_NAME) from where you left off..."
-	@docker-compose start
+	@docker compose start
 
 ## stop	:	Stop containers.
 stop:
 	@echo "Stopping containers for $(PROJECT_NAME)..."
-	@docker-compose stop
+	@docker compose stop
 
 ## prune	:	Remove containers and their volumes.
 ##		You can optionally pass an argument with the service name to prune single container
@@ -38,7 +38,7 @@ stop:
 ##		prune mariadb solr	: Prune `mariadb` and `solr` containers and remove their volumes.
 prune:
 	@echo "Removing containers for $(PROJECT_NAME)..."
-	@docker-compose down -v $(filter-out $@,$(MAKECMDGOALS))
+	@docker compose down -v $(filter-out $@,$(MAKECMDGOALS))
 
 ## ps	:	List running containers.
 ps:
@@ -59,7 +59,7 @@ wp:
 ##		logs php	: View `php` container logs.
 ##		logs nginx php	: View `nginx` and `php` containers logs.
 logs:
-	@docker-compose logs -f $(filter-out $@,$(MAKECMDGOALS))
+	@docker compose logs -f $(filter-out $@,$(MAKECMDGOALS))
 
 # https://stackoverflow.com/a/6273809/1826109
 %:
